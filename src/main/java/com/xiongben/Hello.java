45408b479007e5cdead3048184a47095370c9a58
Xiong Ben <xiongben1993@gmail.com>
2019-12-26 22:26:11 +0800

    [init]

---
+++ src/main/java/com/xiongben/Hello.java
@@ -0,0 +1,11 @@
+package com.xiongben;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Hello {
+    public static void main(String[] args) {
+        SpringApplication.run(Hello.class,args);
+    }
+}
